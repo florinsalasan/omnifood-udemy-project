00483f1daf17d0fe5134334e5e05fd84b8d19f6d
Florin Salasan <florinsalasan@gmail.com>
2022-09-23 14:21:29 -0400

    made a mobile nav menu functional, need to remove some styling from the opening and closing button

--- starter/07-Omnifood-Desktop/script.js
+++ starter/07-Omnifood-Desktop/script.js
@@ -1,3 +1,25 @@
 const yearEle = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEle.textContent = currentYear;
+
+var menuButton = document.querySelector(".nav-menu-btn")
+var navBar = document.querySelector(".navbar")
+var navLinks = document.querySelectorAll('.navbar-link')
+
+menuButton.addEventListener('click', () => {
+  if (navBar.classList.contains('nav-open')) {
+    navBar.classList.remove('nav-open')
+  }
+  else {
+    console.log(navBar.classList.add("nav-open"))
+  }
+})
+
+for (let i = 0; i < navLinks.length; i++) {
+
+  navLinks[i].addEventListener('click', () => {
+    if (navBar.classList.contains('nav-open')) {
+      navBar.classList.remove('nav-open')
+    }
+  })
+}
